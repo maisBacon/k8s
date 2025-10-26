fdd002a62d89d2536856ca9f7bbdc8828d257332
Renan <renann.eiras@gmail.com>
2025-10-26 00:03:20 -0300

    Refactor New Relic configuration in newrelicense.js: update app name and use environment variable for license key

--- newrelicense.js
+++ newrelicense.js
@@ -1,7 +1,7 @@
 'use strict';
 module.exports = {
-  app_name: ['Meu-App-K8s'],  // Nome no dashboard
-  license_key: 'NEW_RELIC_APP_NAME=k8s NEW_RELIC_LICENSE_KEY=b4f962a57a224623c28eca7b518a7b0dFFFFNRAL node -r newrelic server.js',  // Pega no New Relic (grátis trial)
+  app_name: ['k8s-app'],
+  license_key: process.env.NEW_RELIC_LICENSE_KEY,
   logging: {
     level: 'info'
   },
